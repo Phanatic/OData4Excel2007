94fe6c514f6e515adf6215f1b46b2c699f14d1a5
Phani Raj <Phanirajuyn@hotmail.com>
2011-09-06 18:22:45 -0700

    First draft of UI for Excel plugin

---
+++ ExcelAddinUI/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using ExcelAddinUI.Utilities;
+
+namespace ExcelAddinUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ServiceDocumentParser serviceDocParser;
+        ServiceDocument serviceDocument;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        public delegate void DocumentDownloadCompletedDelegate();
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.LoadCollectionsInService("http://odata.netflix.com/v2/Catalog/");
+        }
+
+        private void LoadCollectionsInService(string serviceUri)
+        {
+            serviceDocParser = new ServiceDocumentParser();
+            serviceDocParser.ParseComplete += new ServiceDocumentParser.OnParseComplete(serviceDocParserParseComplete);
+            serviceDocParser.BeginParse(serviceUri);
+        }
+
+        void serviceDocParserParseComplete(ServiceDocument serviceDocument, Exception Error)
+        {
+            if (Error == null)
+            {
+                this.serviceDocument = serviceDocument;
+                Dispatcher.BeginInvoke(new DocumentDownloadCompletedDelegate(DisplayServiceCollections), null);
+            }
+        }
+
+        public void DisplayServiceCollections()
+        {
+            lbCollections.ItemsSource = serviceDocument.EntitySetUris;
+        }
+    }
+}
---
+++ ExcelAddinUI/Utilities/MetadataDocumentParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Data.Edm;
+using System.Net;
+using System.Xml.Linq;
+using Microsoft.Data.Edm.Csdl;
+
+namespace ExcelAddinUI.Utilities
+{
+    public class MetadataDocumentParser
+    {
+        public void BeginParse(string metadataDocumentUri, Action<IEdmModel> onMetadataParsed)
+        {
+            HttpWebRequest metadataRequest = HttpWebRequest.Create(metadataDocumentUri) as HttpWebRequest;
+            metadataRequest.BeginGetResponse
+                ((requestAsyncResult) =>
+                {
+                    var metadataResponse = metadataRequest.EndGetResponse(requestAsyncResult);
+                    var metadataDocument = XDocument.Load(metadataResponse.GetResponseStream());
+                }, null);
+        }
+    }
+}
